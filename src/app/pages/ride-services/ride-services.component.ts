39388fcd4f50923874a907f6a964336087d4c312
Jovica <jovica.dev@gmail.com>
2018-11-26 14:37:26 -0800

    add ride services page and update header

---
+++ src/app/pages/ride-services/ride-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-services',
+  templateUrl: './ride-services.component.html',
+  styleUrls: ['./ride-services.component.scss']
+})
+export class RideServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
