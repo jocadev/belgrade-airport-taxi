b68dd4ac57c156f1cad8f9cd239a58e524336ae1
Jovica <jovica.dev@gmail.com>
2018-12-22 13:40:22 -0800

    Check message for contact form and style

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+firstName: string;
+lastName: string;
+userEmail: string;
+userPhone: number;
+textMessage: string;
+showMessagePanel: boolean;
+
+    showMessage() {
+        if (this.firstName !== '' || this.firstName !== '') {
+            this.showMessagePanel = true;
+        }
+        this.showMessagePanel = true;
+    }
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from '../app-routing.module';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { RideServicesComponent } from './ride-services/ride-services.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here	
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ],
   declarations: [HomeComponent, AboutComponent, ContactComponent, RideServicesComponent]
 })
